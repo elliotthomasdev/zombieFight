64189d8f22dce72a3e2b441fb6832f56e8f4b955
dragonpale1 <64854702+dragonpale1@users.noreply.github.com>
2020-12-06 17:45:06 -0600

    Update Teacher.java

--- src/csc422/csp/edu/Teacher.java
+++ src/csc422/csp/edu/Teacher.java
@@ -1,14 +1,13 @@
 package csc422.csp.edu;
 
-//12/4/2020 
+//12/6/2020 
 //CSC 422 
 //zombie war application
 
 public class Teacher extends Survivor
 {
-    public Teacher(int startingHealth, int damageValue)
+    public Teacher(int startingHealth, int damageValue, int nameCount)
     {
-        super(startingHealth, damageValue);
-        type = "teacher";
+        super(startingHealth, damageValue, nameCount);
     }
 }
